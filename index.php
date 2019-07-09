61508221816d82eb9104ec46770b1a8c396216a0
Evergreen Web Design <info@evergreenwebdesign.com>
2019-07-09 09:31:58 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="expires" content="0">
+	
+	<title>Samuel Starkey</title>
+	<link rel="icon" type="image/ico" href="_images/favicon.ico">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta name="description" content="It smells like Samuel Starkey all up in here.">
+	<meta name="format-detection" content="telephone=no">
+
+	<meta property="og:url" content="http://www.samstarkey.com" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Samuel Starkey" />
+    <meta property="og:image" content="http://www.samstarkey.com/_images/sam-starkey.jpg?<?php echo time(); ?>" />
+    <meta property="og:image:alt" content="Samuel Starkey" />
+    <meta property="og:description" content="It smells like Samuel Starkey all up in here." />
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style.css?<?php echo time(); ?>" type="text/css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+</head>
+<body>
+<div id="hero">
+	<div id="bob">
+		<img src="_images/sam-starkey.jpg">
+		<p>Bob Means<br />
+		is my hero</p>
+		<p class="small-txt">I got'cho website right here bro!</p>
+	</div>
+	
+</div>
+
+<script src="js/scripts.js?<?php echo time(); ?>"></script>
+<script src="http://localhost:35729/livereload.js"></script>	
+</body>
+</html>
